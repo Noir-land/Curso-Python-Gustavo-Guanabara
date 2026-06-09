447f6336617127cf145a9a1182e1023aa033dc35
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:32:49 -0300

    Update ex017  Catetos e Hipotenusa.py

--- Mundo 1/Exercícios/ex017  Catetos e Hipotenusa.py
+++ Mundo 1/Exercícios/ex017  Catetos e Hipotenusa.py
@@ -1,3 +1,8 @@
+"""
+Faça um programa que leia o comprimento do cateto oposto (co) e 
+do cateto adjacente (ca) de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
+"""
+
 o = float(input("O tamanho do cateto oposto: "))
 a = float(input("O tamanho do cateto adjacente: "))
 h = (o ** 2 + a ** 2) ** (1/2)
