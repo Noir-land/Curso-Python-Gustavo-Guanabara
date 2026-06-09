a386cb3d2cae1d8772b4c3c965b812a7b3f4d88a
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:42:56 -0300

    Update ex026 Primeira e última ocorrência de uma string.py

--- Mundo 1/Exercícios/ex026 Primeira e última ocorrência de uma string.py
+++ Mundo 1/Exercícios/ex026 Primeira e última ocorrência de uma string.py
@@ -1,3 +1,10 @@
+"""
+Faça um programa que leia uma frase qualquer e mostre:
+Quantas vezes aparece a letra "a"
+Em que posição ela aparece a primeira vez
+Em que posição ela aparece a última vez
+"""
+
 frase = str(input("Digite uma frase: ")).strip()
 n = frase.upper()
 print("A letra A aparece na posição: {}".format(n.count("A")))
