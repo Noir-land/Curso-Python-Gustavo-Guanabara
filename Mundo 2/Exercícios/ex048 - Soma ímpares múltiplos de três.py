f8dddb86cd5b30136ca46e6bef72c0c1ad8648ef
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 20:04:32 -0300

    Update ex048 - Soma ímpares múltiplos de três.py

--- Mundo 2/Exercícios/ex048 - Soma ímpares múltiplos de três.py
+++ Mundo 2/Exercícios/ex048 - Soma ímpares múltiplos de três.py
@@ -1,3 +1,8 @@
+"""
+Faça um programa que calcule a soma entre todos os números impares que 
+são múltiplos de três (3) e que se encontram no intervalo de 1 até 500.
+"""
+
 s = 0
 con = 0
 for c in range(1, 501, 2):
