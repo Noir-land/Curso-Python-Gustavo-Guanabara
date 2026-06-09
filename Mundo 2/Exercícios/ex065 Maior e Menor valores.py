9833aab72ffc17fbc25249dc63ab1887c8311fff
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 20:28:13 -0300

    Update ex065 Maior e Menor valores.py

--- Mundo 2/Exercícios/ex065 Maior e Menor valores.py
+++ Mundo 2/Exercícios/ex065 Maior e Menor valores.py
@@ -1,3 +1,10 @@
+"""
+Crie um programa que leia vários números inteiros pelo teclado
+No final da execução, mostre a média entre todos os valores
+e qual foi o maior e o menor valores lidos
+O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores
+"""
+
 num = soma = cont = div = maior = menor = 0
 pergunta = 'S'
 while pergunta in "sS":
