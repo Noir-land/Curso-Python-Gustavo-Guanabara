df434ac8602ef0890ccc8f0a415081b1b96524e4
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:15:03 -0300

    Update ex004v2 Dissecando uma Variável.py

--- Mundo 1/Exercícios/ex004v2 Dissecando uma Variável.py
+++ Mundo 1/Exercícios/ex004v2 Dissecando uma Variável.py
@@ -1,3 +1,9 @@
+"""
+Faça um programa que leia algo pelo teclado 
+e mostre na tela o seu tipo primitivo
+e todas as informações possíveis sobre ele
+"""
+
 a = input('Digite algo: ')
 print('É um numero interiro ? ', a.isnumeric())
 print('Tem letra ? ', a.isalpha())
