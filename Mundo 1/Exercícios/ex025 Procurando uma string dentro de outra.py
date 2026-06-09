7ca3825e6ab681c6e90ba67d00a0102fef734f20
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:42:19 -0300

    Update ex025 Procurando uma string dentro de outra.py

--- Mundo 1/Exercícios/ex025 Procurando uma string dentro de outra.py
+++ Mundo 1/Exercícios/ex025 Procurando uma string dentro de outra.py
@@ -1,2 +1,7 @@
+"""
+Crie um programa que leia o nome de uma pessoa e 
+diga se ela tem "Silva" no nome
+"""
+
 nome = str(input("Digite um nome: ")).strip()
 print("Você tem Dias no nome ? {}".format('Dias' in nome.title()))
