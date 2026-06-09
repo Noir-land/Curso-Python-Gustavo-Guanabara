fa4f23e37b784b3c9429b9d32f63691403ddbf6c
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 20:16:09 -0300

    Update ex053 Detector de Palíndromo.py

--- Mundo 2/Exercícios/ex053 Detector de Palíndromo.py
+++ Mundo 2/Exercícios/ex053 Detector de Palíndromo.py
@@ -1,3 +1,14 @@
+""" 
+Crie um programa que leia uma frase qualquer
+E diga se ela é um palíndromo, desconsiderando os espaços.
+Exemplos:
+Após a sopa
+A sacada da casa
+A torre da derrota
+o lobo ama o bolo
+Anotaram a data da maratona
+"""
+
 frase = str(input("Digite uma frase: ")).strip().upper()
 dividir = frase.split()
 junto = ''.join(dividir)
