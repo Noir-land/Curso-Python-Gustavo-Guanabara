6ba236a85f576c549326853068ec515c486df09a
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:56:50 -0300

    Update ex036 Aprovando Empréstimo.py

--- Mundo 2/Exercícios/ex036 Aprovando Empréstimo.py
+++ Mundo 2/Exercícios/ex036 Aprovando Empréstimo.py
@@ -1,3 +1,15 @@
+'''
+Escreva um programa para aprovar o empréstimo bancário
+para a compra de uma casa
+O programa vai perguntar o valor da casa, o salário do comprador e em quantos
+anos ele vai pagar
+
+Calcule o valor da prestação mensal,
+sabendo que ela não pode exceder 30% do salário
+ou então o empréstimo será negado
+'''
+
+
 casa = float(input("Valor da Casa: "))
 salario = float(input("Salario: "))
 tempo = int(input("Deseja pagar em ate quantos anos? "))
