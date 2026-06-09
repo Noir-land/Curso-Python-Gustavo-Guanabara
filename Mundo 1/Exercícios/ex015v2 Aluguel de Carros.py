2b98fbb45245daa8ea0dafea23d754436589486e
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:30:22 -0300

    Update ex015v2 Aluguel de Carros.py

--- Mundo 1/Exercícios/ex015v2 Aluguel de Carros.py
+++ Mundo 1/Exercícios/ex015v2 Aluguel de Carros.py
@@ -1,3 +1,9 @@
+"""
+Aluguel de carros:
+Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado
+Calcule o preço a pagar, sabendo que o carro custa R$ 60 por dia e R$ 0.15 por km rodado
+"""
+
 d = float(input("Dias de uso:"))
 k = float(input("KM percorrido: "))
 r = (d*60) + (k*0.15)
