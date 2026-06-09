bb8415a84193e95a37dd67db89f390e8d4770d42
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:55:13 -0300

    Update ex034 Aumentos múltiplos.py

--- Mundo 1/Exercícios/ex034 Aumentos múltiplos.py
+++ Mundo 1/Exercícios/ex034 Aumentos múltiplos.py
@@ -1,3 +1,9 @@
+"""
+Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento
+Para salários superiores a R$ 1.250,00, calcule um aumento de 10%
+Para os inferiores ou iguais, o aumento é de R$ 15%.
+"""
+
 v = float(input("Valor do salario: "))
 a = v * 10 / 100
 r1 = a + v
