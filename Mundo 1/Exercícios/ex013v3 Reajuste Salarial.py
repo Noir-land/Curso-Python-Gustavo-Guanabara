b19a68e19ed8a4986211aad9f342bf3959ea60cf
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:27:42 -0300

    Update ex013v3 Reajuste Salarial.py

--- Mundo 1/Exercícios/ex013v3 Reajuste Salarial.py
+++ Mundo 1/Exercícios/ex013v3 Reajuste Salarial.py
@@ -1,3 +1,8 @@
+"""
+Faça um algoritmo que leia o salário de um funcionário e
+mostre seu novo salário, com 15% de aumento
+"""
+
 s = float(input("Digite o seu salario: R$ "))
 p = float(input("Digite a % para aplicar visualizar o novo salario: "))
 print("Seu salario é de {} com o reajuste de {}% é igual a {}".format(s, p, (p/100*s+s)))
