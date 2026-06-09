de4a90dac9ec6e80b3ae95db138b90cc4010620f
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:46:58 -0300

    Update ex031v2 Custo da Viagem.py

--- Mundo 1/Exercícios/ex031v2 Custo da Viagem.py
+++ Mundo 1/Exercícios/ex031v2 Custo da Viagem.py
@@ -1,3 +1,9 @@
+"""
+Desenvolva um programa que pergunte a distância de uma viagem em Km.
+Calcule o preço da passagem, cobrando R$ 0,50 por Km para viagens de até 200km
+e R$ 0,45 para viagens mais longas
+"""
+
 '''d = float(input("Digite a distancia a percorer: "))
 if d <= 200:
     v = d * 0.50
