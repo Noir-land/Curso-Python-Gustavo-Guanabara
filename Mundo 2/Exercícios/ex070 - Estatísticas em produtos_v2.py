b99d13555c1684d07cd5d43b4764ab43dad4c1fc
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 20:34:04 -0300

    Update ex070v2 - Estatísticas em produtos.py

--- Mundo 2/Exercícios/ex070 - Estatísticas em produtos_v2.py
+++ Mundo 2/Exercícios/ex070 - Estatísticas em produtos_v2.py
@@ -1,3 +1,12 @@
+"""
+Crie um programa que leia o nome e o preço de vários produtos.
+O programa deverá perguntar se o usuário vai continuar.
+No final, mostre:
+a) qual é o total gasto na compra
+b) quantos produtos custam mais de R$ 1000
+c) qual é o nome do produto mais barato
+"""
+
 print("*"*40)
 print('{:^40}'.format("NOIR SUPERMERCADO"))
 print("*"*40)
