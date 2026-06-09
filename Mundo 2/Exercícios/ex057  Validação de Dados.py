2ee673a197c29c0bdbfd1d19a0523cb805c7d2fa
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 20:19:04 -0300

    Update ex057  Validação de Dados.py

--- Mundo 2/Exercícios/ex057  Validação de Dados.py
+++ Mundo 2/Exercícios/ex057  Validação de Dados.py
@@ -1,3 +1,11 @@
+"""
+Desenvolva um programa que leia nome, idade e sexo de 4 pessoas
+No final do programa, mostre:
+A média de idade do grupo
+Qual é o nome do homem mais velho
+Quantas mulheres têm menos de 20 anos
+"""
+
 sex = str(input("Digite seu sexo: ")).upper().strip()[0]
 while sex not in "MnFf":  # QUANDO TEM " "MnFf" É FINALIZADO O LOOP ou QUANDO A AFIRMAÇÃO SE TORNA FALSA
     sex = str(input("Digite seu sexo corretamente: ")).upper().strip()[0]
