7959dd60582d74918272b64b6fa3386111c6600e
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:31:26 -0300

    Update ex016 Quebrando um número.py

--- Mundo 1/Exercícios/ex016 Quebrando um número.py
+++ Mundo 1/Exercícios/ex016 Quebrando um número.py
@@ -1,3 +1,8 @@
+"""
+Crie um programa que leia um número real qualquer 
+pelo teclado e mostre na tela a sua porção inteira.
+""""
+
 from math import floor
 n = float(input("Digite um numero: "))
 print("O valor digitado foi {} e seu numero inteiro é {}".format(n, floor(n)))
