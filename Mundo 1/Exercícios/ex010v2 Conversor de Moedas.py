17cfefa9e106870feb2ff4f8b3733be71d83996c
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:24:16 -0300

    Update ex010v2 Conversor de Moedas.py

--- Mundo 1/Exercícios/ex010v2 Conversor de Moedas.py
+++ Mundo 1/Exercícios/ex010v2 Conversor de Moedas.py
@@ -1,3 +1,8 @@
+"""
+Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e
+mostre quantos dólares ela pode comprar
+"""
+
 n = float(input("Coloque o valor que deseja converter de reais para dolar: R$"))
 dolar = n / 4.28
 print("R$:{:.2f} é convertido para USS:{:.2f}".format(n, dolar))
