25ec0b874d633e22c41c874cb834e88565acbe25
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:32:02 -0300

    Update ex016v2 Quebrando um número.py

--- Mundo 1/Exercícios/ex016v2 Quebrando um número.py
+++ Mundo 1/Exercícios/ex016v2 Quebrando um número.py
@@ -1,2 +1,7 @@
+"""
+Crie um programa que leia um número real qualquer 
+pelo teclado e mostre na tela a sua porção inteira.
+"""
+
 num = float(input("Digite um numero: "))
 print("O numero:{} tem sua parte inteira igual a {}".format(num, int(num)))
