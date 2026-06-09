e2780d521cb6d5b572973d4120d8ba72ae39ec0f
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:58:45 -0300

    Update ex039 Alistamento Militar.py

--- Mundo 2/Exercícios/ex039 Alistamento Militar.py
+++ Mundo 2/Exercícios/ex039 Alistamento Militar.py
@@ -1,3 +1,13 @@
+'''
+Faça um programa que leia o ano de nascimento de um jovem e informe
+de acordo com sua idade:
+Se ele ainda vai se alistar ao serviço militar
+Se é a hora de se alistar
+Se já passou do tempo do alistamento
+Seu programa também deverá mostrar o tempo que faltou ou que passou do prazo
+'''
+
+
 import datetime
 ano = int(input("Em que ano você nasceu? "))
 idade = datetime.date.today().year - ano
