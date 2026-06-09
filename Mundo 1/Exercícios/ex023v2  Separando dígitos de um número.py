ef98c4a6a5966a181e426470a097f0766df6641e
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:40:07 -0300

    Update ex023v2  Separando dígitos de um número.py

--- Mundo 1/Exercícios/ex023v2  Separando dígitos de um número.py
+++ Mundo 1/Exercícios/ex023v2  Separando dígitos de um número.py
@@ -1,3 +1,12 @@
+"""
+Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados
+exemplo: digite um número: 1834
+unidade: 4
+dezenas: 3
+centenas: 8
+milhares: 1
+"""
+
 n = int(input("Digite um numero de 0 a 9999 "))
 u = n // 1 % 10
 d = n // 10 % 10
