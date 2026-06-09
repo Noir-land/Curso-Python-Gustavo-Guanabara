aa7f32fd1313c956312750ae9698781075a6ce0e
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:26:48 -0300

    Update ex013 Reajuste Salarial.py

--- Mundo 1/Exercícios/ex013 Reajuste Salarial.py
+++ Mundo 1/Exercícios/ex013 Reajuste Salarial.py
@@ -1,3 +1,7 @@
+"""Faça um algoritmo que leia o salário de um funcionário e
+mostre seu novo salário, com 15% de aumento
+"""
+
 n = float(input("Digite o valor do seu salarario para o reajuste:R$"))
 r = n*15/100
 r2 = r + n
