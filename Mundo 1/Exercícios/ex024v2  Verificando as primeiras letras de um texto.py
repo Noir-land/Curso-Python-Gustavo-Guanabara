91e1a293a9f627d4b76e8b5844a9a0563e2a94b5
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:41:49 -0300

    Update ex024v2  Verificando as primeiras letras de um texto.py

--- Mundo 1/Exercícios/ex024v2  Verificando as primeiras letras de um texto.py
+++ Mundo 1/Exercícios/ex024v2  Verificando as primeiras letras de um texto.py
@@ -1,2 +1,8 @@
+"""
+Crie um programa que leia o nome de uma cidade e
+diga se ela começa ou não com a palavra "Santo"
+"""
+
+
 cidade = str(input("Digite o nome da sua cidade:")).strip()
 print(cidade[:5].upper() == "SANTO")
