eed6d0fe0521115d191c950b3e61257c59225d13
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:16:10 -0300

    Update ex005v2 Antecessor e Sucessor.py

--- Mundo 1/Exercícios/ex005v2 Antecessor e Sucessor.py
+++ Mundo 1/Exercícios/ex005v2 Antecessor e Sucessor.py
@@ -1,2 +1,7 @@
+"""
+Faça um programa que leia um número inteiro e
+mostre na tela o seu sucessor e seu antecessor
+"""
+
 n = int(input("Digite um numero: "))
 print("Sabendo que o numero é  {}, o seu antecessor é  {} e seu sucessor é  {}.".format(n,(n-1), (n+1)))
