2c5712464e932a73c0724215c449e0bb98f93713
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:55:59 -0300

    Update ex035  Analisando Triângulo v1.0.py

--- Mundo 1/Exercícios/ex035  Analisando Triângulo v1.0.py
+++ Mundo 1/Exercícios/ex035  Analisando Triângulo v1.0.py
@@ -1,3 +1,10 @@
+"""
+Desenvolva um programa que leia o comprimento de três retas e
+diga ao usuário se elas podem ou não formar um triângulo
+Inclusive posso dizer qual tipo de triângulo pode ser formado.
+Não deve ser difícil isso em Python.
+"""
+
 a = float(input("Primeira reta: "))
 b = float(input("Segunda  reta: "))
 c = float(input("Terceira reta: "))
