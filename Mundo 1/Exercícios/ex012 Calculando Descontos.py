e761c64052cde56afa98c5f04f44690136666b62
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:26:01 -0300

    Update ex012 Calculando Descontos.py

--- Mundo 1/Exercícios/ex012 Calculando Descontos.py
+++ Mundo 1/Exercícios/ex012 Calculando Descontos.py
@@ -1,3 +1,8 @@
+"""
+Faça um algoritmo que leia o preço de um produto e 
+mostre seu novo preço, com 5% de desconto
+"""
+
 n = float(input("Digite o valor do produto: R$"))
 r = 5/100 * n
 r2 = n - r
