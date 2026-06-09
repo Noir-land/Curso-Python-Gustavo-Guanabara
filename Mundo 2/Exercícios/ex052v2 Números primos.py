95aa3e59dc40777a5d04a6d9fcf79b4bcd3adffd
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 20:14:02 -0300

    Update ex052v2 Números primos.py

--- Mundo 2/Exercícios/ex052v2 Números primos.py
+++ Mundo 2/Exercícios/ex052v2 Números primos.py
@@ -1,3 +1,8 @@
+"""
+Faça um programa que leia um número inteiro e
+diga se ele é ou não um número primo
+"""
+
 num = int(input("Digite um numero: "))
 for c in range(1, num + 1):
     if num % c == 0:
