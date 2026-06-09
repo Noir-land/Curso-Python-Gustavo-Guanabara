c6721a9e2feb53a39e254bb03ef67f2044810237
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:28:38 -0300

    Update ex014v2 Conversor de Temperaturas.py

--- Mundo 1/Exercícios/ex014v2 Conversor de Temperaturas.py
+++ Mundo 1/Exercícios/ex014v2 Conversor de Temperaturas.py
@@ -1,3 +1,8 @@
+"""
+Conversor de temperaturas: escreva um programa 
+que converta uma temperatura digitada em ºC para ºF
+"""
+
 t = float(input("Converter Fahrenheit para Celsius: "))
 r = (t-32) * 5 / 9
 print("\033[1;31m{}ºF\033[m é convertido para \033[1;31m{:.2f}ºC".format(t, r))
