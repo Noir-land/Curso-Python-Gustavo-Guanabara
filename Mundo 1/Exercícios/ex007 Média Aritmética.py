5cb0d09d83f348a5b3beaaf9a3f26ebcd8f011f4
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:18:26 -0300

    Update ex007 Média Aritmética.py

--- Mundo 1/Exercícios/ex007 Média Aritmética.py
+++ Mundo 1/Exercícios/ex007 Média Aritmética.py
@@ -1,3 +1,8 @@
+"""
+Desenvolva um programa que leia as 
+duas notas de um aluno, calcule e mostre a sua média
+"""
+
 n1 = float(input("Digite a primeira nota: "))
 n2 = float(input("Digite a segudna nota: "))
 n3 = float(input("Digite a terceira nota: "))
