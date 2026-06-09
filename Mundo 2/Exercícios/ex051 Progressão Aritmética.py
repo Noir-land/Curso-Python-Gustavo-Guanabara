512105abbc62f9b387d0c4b743652df36a3ad1b5
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 20:13:03 -0300

    Update ex051 Progressão Aritmética.py

--- Mundo 2/Exercícios/ex051 Progressão Aritmética.py
+++ Mundo 2/Exercícios/ex051 Progressão Aritmética.py
@@ -1,11 +1,12 @@
-"""Desenvolva um programa que leia o primeiro termo e a razão
+"""
+Desenvolva um programa que leia o primeiro termo e a razão
 de uma PA (Progressão Aritmética).
 No final, mostre os 10 primeiros termos dessa progressão.
 """
 
 prim = int(input("Digite o primeiro termo: "))
 raz = int(input("Razão: "))
-dt = prim + (10-1) * raz
-for c in range(prim, dt+raz, raz):
+dt = prim + (10 - 1) * raz
+for c in range(prim, dt + raz, raz):
     print(f"{c}", end=">>")
 print("Acabou")
