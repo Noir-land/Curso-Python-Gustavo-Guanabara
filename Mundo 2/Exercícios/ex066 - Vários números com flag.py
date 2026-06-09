3efd3ca433107bcf58c2116e019afa0eec2cc7e0
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 20:28:43 -0300

    Update ex066 - Vários números com flag.py

--- Mundo 2/Exercícios/ex066 - Vários números com flag.py
+++ Mundo 2/Exercícios/ex066 - Vários números com flag.py
@@ -1,3 +1,11 @@
+"""
+Crie um programa que leia vários números inteiros pelo teclado
+O programa só vai parar quando o usuário digitar 999,
+que é a condição de parada (flag).
+No final, mostre quantos números foram digitados
+e qual foi a acc entre eles (desconsiderando o flag)
+"""
+
 n = s = cont = 0
 while True:
     n = int(input("Digite um numero.[999 para parar]"))
