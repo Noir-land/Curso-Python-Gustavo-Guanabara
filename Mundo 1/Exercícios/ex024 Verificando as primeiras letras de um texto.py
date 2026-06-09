d223519f991bca85b7133bdafda5fa7a03689fab
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:41:30 -0300

    Update ex024 Verificando as primeiras letras de um texto.py

--- Mundo 1/Exercícios/ex024 Verificando as primeiras letras de um texto.py
+++ Mundo 1/Exercícios/ex024 Verificando as primeiras letras de um texto.py
@@ -1,7 +1,11 @@
-"""frase = str(input("Digite seu nome:")).strip()
-n = frase.upper()
-print("Existem {} letras ao todo".format(n.find("Santo")))
 """
+Crie um programa que leia o nome de uma cidade e
+diga se ela começa ou não com a palavra "Santo"
+"""
+
+'''frase = str(input("Digite seu nome:")).strip()
+n = frase.upper()
+print("Existem {} letras ao todo".format(n.find("Santo")))'''
 frase = str(input("nome ")).strip()
 n0 = frase.title()
 n = "Santo" in n0[:5]
