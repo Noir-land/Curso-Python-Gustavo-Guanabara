1b363f6c568bc3a5f4cd51a7cec8f7b9b7a1e608
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 20:16:45 -0300

    Update ex054  Grupo da Maioridade.py

--- Mundo 2/Exercícios/ex054  Grupo da Maioridade.py
+++ Mundo 2/Exercícios/ex054  Grupo da Maioridade.py
@@ -1,3 +1,9 @@
+"""
+Crie um programa que leia o ano de nascimento de sete pessoas
+No final, mostre quantas pessoas ainda não atingiram a maioridade
+e quantas já são maiores
+"""
+
 from datetime import date
 atual = date.today().year
 menores = 0
