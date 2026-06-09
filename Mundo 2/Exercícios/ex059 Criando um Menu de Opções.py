12ad60ce7c4f16fead47f461dc4462adb4545042
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 20:24:24 -0300

    Update ex059 Criando um Menu de Opções.py

--- Mundo 2/Exercícios/ex059 Criando um Menu de Opções.py
+++ Mundo 2/Exercícios/ex059 Criando um Menu de Opções.py
@@ -1,3 +1,15 @@
+"""
+Crie um programa que leia dois valores e mostre um menu
+na tela:
+1: somar
+2: multiplicar
+3: maior
+4: novos números
+5: sair do programa
+Seu programa deverá realizar a operação solicitada em cada caso 
+"""
+
+
 n1 = int(input("Digite um numero: "))
 n2 = int(input("digite um numero: "))
 maior = n1
