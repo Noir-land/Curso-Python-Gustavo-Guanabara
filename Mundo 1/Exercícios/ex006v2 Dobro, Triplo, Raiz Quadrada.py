0b94fbb663ae2b5e3aa5bc9ade900f470481c559
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:17:02 -0300

    Update ex006v2 Dobro, Triplo, Raiz Quadrada.py

--- Mundo 1/Exercícios/ex006v2 Dobro, Triplo, Raiz Quadrada.py
+++ Mundo 1/Exercícios/ex006v2 Dobro, Triplo, Raiz Quadrada.py
@@ -1,3 +1,8 @@
+"""
+Crie um algoritmo que leia um número e 
+mostre o seu dobro, o seu triplo e sua raiz quadrada
+"""
+
 n = int(input("Digite um numero: "))
 print("O dobro de {} é {}".format(n, n*2), end=". ")
 print("O triplo de {} é {}".format(n, n*3), end=" e ")
