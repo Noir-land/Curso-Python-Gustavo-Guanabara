4c692501d832fb55fbe403cd3402a797d6a8adbd
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:35:34 -0300

    Update ex020 Sorteando uma ordem na lista.py

--- Mundo 1/Exercícios/ex020 Sorteando uma ordem na lista.py
+++ Mundo 1/Exercícios/ex020 Sorteando uma ordem na lista.py
@@ -1,3 +1,8 @@
+"""
+O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos.
+ Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+"""
+
 import random
 a1 = input("Primeiro aluno: ")
 a2 = input("Segundo aluno: ")
