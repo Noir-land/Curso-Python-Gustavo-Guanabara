5f994c153d0b1a2d285b1001875a367cf757cc64
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-09 19:25:03 -0300

    Update ex011 Pintando Parede.py

--- Mundo 1/Exercícios/ex011 Pintando Parede.py
+++ Mundo 1/Exercícios/ex011 Pintando Parede.py
@@ -1,3 +1,7 @@
+"""Faça um programa que leia a largura e a algura de uma parede em metros, calcule a sua área e
+a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m²
+"""
+
 n = float(input("Largura da parede: "))
 n2 = float(input("Altura da parede: "))
 r = n*n2
