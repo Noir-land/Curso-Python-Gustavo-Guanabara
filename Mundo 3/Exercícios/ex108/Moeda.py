1f43b9544cd7b93dc2735c42c4735685d964e86c
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-15 18:34:12 -0300

    Update Moeda.py

--- Mundo 3/Exercícios/ex108/Moeda.py
+++ Mundo 3/Exercícios/ex108/Moeda.py
@@ -4,39 +4,24 @@ def moeda(msg):
 
 def aumentar(msg, sit=False):
     msg *= 2
-    if sit:
-        return f'{moeda(msg)}'
-    else:
-        return msg
+    return msg
 
 
 def diminuir(msg, sit=False):
     msg -= msg
-    if sit:
-        return f'{moeda(msg)}'
-    else:
-        return msg
+    return msg
 
 
 def dividir(msg, sit=False):
     msg /= 2
-    if sit:
-        return f'{moeda(msg)}'
-    else:
-        return msg
+    return msg
 
 
 def porcento(msg, p=0, sit=False):
     msg = msg / 100 * p
-    if sit:
-        return f'{moeda(msg)}'
-    else:
-        return msg
+    return msg
 
 
 def menos_porcento(msg, p=0, sit=False):
     msg = msg - (msg / 100 * p)
-    if sit:
-        return f'{moeda(msg)}'
-    else:
-        return msg
+    return msg
