89280020073e5b429a8649312576818dbc292d5e
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-15 15:28:58 -0300

    Update and rename temp to Moeda.py

---
+++ Mundo 3/Exercícios/ex107/Moeda.py
@@ -0,0 +1,39 @@
+
+def aumentar(msg, sit=False):
+    msg *= 2
+    if sit:
+        return f'{moeda(msg)}'
+    else:
+        return msg
+
+
+def diminuir(msg, sit=False):
+    msg -= msg
+    if sit:
+        return f'{moeda(msg)}'
+    else:
+        return msg
+
+
+def dividir(msg, sit=False):
+    msg /= 2
+    if sit:
+        return f'{moeda(msg)}'
+    else:
+        return msg
+
+
+def porcento(msg, p=0, sit=False):
+    msg = msg / 100 * p
+    if sit:
+        return f'{moeda(msg)}'
+    else:
+        return msg
+
+
+def menos_porcento(msg, p=0, sit=False):
+    msg = msg - (msg / 100 * p)
+    if sit:
+        return f'{moeda(msg)}'
+    else:
+        return msg
