d949058c953ea5aa40074a1991ac2cf2de91da09
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-15 16:20:19 -0300

    Update ex109 - Formatando moedas em Python .py

--- Mundo 3/Exercícios/ex109/ex109 - Formatando moedas em Python .py
+++ Mundo 3/Exercícios/ex109/ex109 - Formatando moedas em Python .py
@@ -1,10 +1,9 @@
-from utilitariosCV_exercicio_110 import  Moeda, cabecalho
+import  Moeda
 
 valor = int(input('Digite um valor: '))
-cabecalho.titulo('-=')
+print()
 print(f'O dobro de {Moeda.moeda(valor)} é {Moeda.aumentar(valor, True)}')
 print(f'O {Moeda.moeda(valor)} menos {valor} é {Moeda.diminuir(valor, True)}')
 print(f'A metade de {Moeda.moeda(valor) } é {Moeda.dividir(valor, True)}')
 print(f'10% de {Moeda.moeda(valor)} é {Moeda.porcento(valor, 10, True)}')
 print(f'O valor {Moeda.moeda(valor)} com menos 19% é {Moeda.menos_porcento(valor, 19, True)}')
-cabecalho.titulo('-=')
