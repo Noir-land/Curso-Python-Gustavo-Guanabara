29f2616c9618c8ed83d0ac320c8d1e5bebc8bcc5
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-15 15:15:59 -0300

    Update ex107 - Exercitando módulos em Python.py

--- Mundo 3/Exercícios/ex107/ex107 - Exercitando módulos em Python.py
+++ Mundo 3/Exercícios/ex107/ex107 - Exercitando módulos em Python.py
@@ -1,11 +1,11 @@
-from utilitariosCV_exercicio_110 import Moeda, cabecalho
+import Moeda
 
 
 valor = int(input('Digite um valor: '))
-cabecalho('-=')
+print()
 print(f'O dobro de {valor} é {Moeda.aumentar(valor)}')
 print(f'O {valor} menos {valor} é {Moeda.diminuir(valor)}')
 print(f'A metade de {valor} é {Moeda.dividir(valor)}')
 print(f'10% de {valor} é {Moeda.porcento(valor, 10)}')
 print(f'O valor {valor} com menos 19% é {Moeda.menos_porcento(valor, 19)}')
-cabecalho('-=')
+print(
