a5a110f9940a40217ae91e7bf5893a6c18379ffb
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-15 15:59:00 -0300

    Add files via upload

---
+++ Mundo 3/Exercícios/ex108/ex108 - Formatando moedas em Python.py
@@ -0,0 +1,17 @@
+
+"""
+Adapte o código do desafio 107, criando uma função adicional chamada 'moeda()' que consiga mostrar os valores como um valor monetário formatado.
+"""
+
+
+from utilitariosCV_exercicio_110 import  Moeda,cabecalho
+
+
+valor = int(input('Digite um valor: '))
+cabecalho.titulo('-=')
+print(f'O dobro de {Moeda.moeda(valor)} é {Moeda.moeda(Moeda.aumentar(valor))}')
+print(f'O {Moeda.moeda(valor)} menos {valor} é {Moeda.moeda(Moeda.diminuir(valor))}')
+print(f'A metade de {Moeda.moeda(valor) } é {Moeda.moeda(Moeda.dividir(valor))}')
+print(f'10% de {Moeda.moeda(valor)} é {Moeda.moeda(Moeda.porcento(valor, 10))}')
+print(f'O valor {Moeda.moeda(valor)} com menos 19% é {Moeda.moeda(Moeda.menos_porcento(valor, 19))}')
+cabecalho.titulo('-=')
