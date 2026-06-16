fe501a4ef77a6e178ccf13058d5e65928d026292
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-16 09:23:32 -0300

    Update ex110 - Reduzindo ainda mais seu programa .py

--- Mundo 3/Exercícios/ex110/ex110 - Reduzindo ainda mais seu programa .py
+++ Mundo 3/Exercícios/ex110/ex110 - Reduzindo ainda mais seu programa .py
@@ -1,3 +1,8 @@
+"""
+Adicione ao módulo moeda.py, criado nos exercícios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.
+"""
+
+
 from utilitariosCV_exercicio_110 import  Moeda
 
 valor = int(input('Digite um valor: '))
