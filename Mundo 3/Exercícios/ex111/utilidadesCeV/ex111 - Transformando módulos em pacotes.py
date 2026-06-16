a174d7a74f86b302c506a4f5b92681da742c4dc3
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-16 11:27:06 -0300

    Add files via upload

---
+++ Mundo 3/Exercícios/ex111/utilidadesCeV/ex111 - Transformando módulos em pacotes.py
@@ -0,0 +1,5 @@
+from utilitariosCV_exercicio_110.dados import leia_dinheiro
+from utilitariosCV_exercicio_110.Moeda import resumo
+
+valor = leia_dinheiro('Digite um valor: ')
+resumo(valor, 80, 35)
