da0562983058276c7166294e1e1fb330f4e30574
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-17 10:58:40 -0300

    Update __init__.py

--- Mundo 3/Exercícios/ex112/utilidadesCeV/dado/__init__.py
+++ Mundo 3/Exercícios/ex112/utilidadesCeV/dado/__init__.py
@@ -1,1 +1,10 @@
+def leia_dinheiro(msg):
+    while True:
+        variavel = str(input(msg)).replace(',', '.').strip()
+        if variavel.isnumeric():
+            return int(variavel)
+        if variavel.replace('.', '', 1).isdigit():
+            return float(variavel)
+        else:
+            print(f'ERRO! \'{variavel}\' NÃO É VALIDO. Digite um número valido')
 
