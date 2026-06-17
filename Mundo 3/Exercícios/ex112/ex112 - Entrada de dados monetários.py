0c22e2d443b3afee152d95af2f18f50ee7d3b01a
Noir-land <156199278+Noir-land@users.noreply.github.com>
2026-06-17 11:03:18 -0300

    Add files via upload

---
+++ Mundo 3/Exercícios/ex112/ex112 - Entrada de dados monetários.py
@@ -0,0 +1,12 @@
+"""
+Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo chamado dado. 
+Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como a função input(), 
+mas com uma validação de dados para aceitar apenas valores que sejam monetários.
+"""
+
+
+from utilitariosCV_exercicio_110.dados import leia_dinheiro
+from utilitariosCV_exercicio_110.Moeda import resumo
+
+valor = leia_dinheiro('Digite um valor: ')
+resumo(valor, 80, 35)
